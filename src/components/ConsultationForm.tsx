1dc2922026a1d80c707943992c16212a8a6a278e
19Erdem03 <erdemerolsuer@gmail.com>
2025-10-13 23:18:03 +0300

    Update ConsultationForm.tsx

--- src/components/ConsultationForm.tsx
+++ src/components/ConsultationForm.tsx
@@ -61,7 +61,7 @@ export default function ConsultationForm({ onBack, onHome, selectedService = '' 
     
     try {
       // Send form data to Make.com webhook
-      const webhookResponse = await fetch('https://n8n.n8nerdem.org/webhook/f07b4b07-cd69-4ae8-bab4-ee8741d0fed9', {
+      const webhookResponse = await fetch('https://n8n.n8nerdem.org/webhook-test/f07b4b07-cd69-4ae8-bab4-ee8741d0fed9', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
