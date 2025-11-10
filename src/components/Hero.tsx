1fe0f860216bc3ac9774c5a935afdfae806229be
19Erdem03 <erdemerolsuer@gmail.com>
2025-11-10 20:19:28 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,7 @@ function Hero({ onBookCall }) {
             >
               <div className="flex items-center justify-center gap-3">
                 <ArrowUp className="w-5 h-5" />
-                <span>Book Call</span>
+                <span>Get Your Free Demo</span>
               </div>
             </button>
 
